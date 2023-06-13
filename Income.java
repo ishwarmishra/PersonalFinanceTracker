993ec1ee6e4257daf308ccd1ed54559cbf944db3
ishwarmishra <thisismeipm123@gmail.com>
2023-06-13 13:59:30 +0545

    commit change

--- Income.java
+++ Income.java
@@ -1,45 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pft;
 import java.util.Date;
-import java.util.concurrent.atomic.AtomicInteger;
+
 
 public class Income implements IncomeExpensedetails {
     
     
-    private static final AtomicInteger count=new AtomicInteger(0);
-    private Date date;
-    private int category_id;
-    private float amount;
-    private String description;
-    
-    public Income(){
-            }
- 
+   private int id;
+   private Date date;
+   private int category_id;
+   private float amount;
+   private String description;
    
+   public Income(int id, Date date, int category_id, float amount, String description){
+       this.id=id;
+       this.date=date;
+       this.category_id=category_id;
+       this.amount=amount;
+       this.description=description;
+   }
     
+    
+
     @Override
-    public void viewDetails() {
+    public void add(int id, Date date, int category_id, float amount, String description) {
+        throw new UnsupportedOperationException("Not supported yet."); 
         
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
+        
     }
 
     @Override
-    public void edit() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void edit(int id) {
+        throw new UnsupportedOperationException("Not supported yet.");  
+        
     }
 
     @Override
-    public void add() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void viewDetails(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        
     }
 
     @Override
     public void back() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); 
+        
     }
+
+        
+    
+   
+    
     
     
 }
--- IncomeExpensedetails.java
+++ IncomeExpensedetails.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-package pft;
 
-/**
- *
- * @author ishwar
- */
+package pft;
+import java.util.Date;
+import java.util.UUID;
+
 public interface IncomeExpensedetails {
+    
+    String generatedUniqueId();
+    
+    void add(int id,Date date,int category_id,float amount,String description);
+    void edit(int id);
+    
+    void viewDetails(int id);
+    void back();
     
 }
