e6bb1f6e2ee0e7d4ac8400f88fe6a4c804b2e225
ishwarmishra <thisismeipm123@gmail.com>
2023-06-13 12:39:35 +0545

    first commit

---
+++ Dashboard.java
@@ -0,0 +1,55 @@
+
+package pft;
+
+import java.util.Scanner;
+
+
+public class Dashboard {
+    
+    public static void printMenu(String[] options){
+        System.out.println("Dashboard");
+        for (String option : options){
+            System.out.println(option);
+        }
+        System.out.print("Choose your option : ");
+    }
+    public static void main(String[] args) {
+        String[] options = {"1- Income",
+                            "2- Expense",
+                            "3- Logout",
+                            
+        };
+        Scanner scanner = new Scanner(System.in);
+        int option = 1;
+        while (option!=3){
+            printMenu(options);
+            try {
+                option = scanner.nextInt();
+                switch (option){
+                    case 1: income(); break;
+                    
+                    case 2: expense(); break;
+                    
+                    case 3: logout(); 
+                    
+                }
+            }
+            catch (Exception ex){
+                System.out.println("Please enter an integer value between 1 and " + options.length);
+                scanner.next();
+            }
+        }
+    }
+
+    private static void income() {
+        System.out.println("Welcome in Income Details");
+    }
+    private static void expense() {
+        System.out.println("Welcome in Expense Details");
+    }
+    private static void logout() {
+        System.out.println("Logging out");
+    }
+
+    
+}
---
+++ Login.java
@@ -0,0 +1,37 @@
+package pft;
+
+
+import java.util.Scanner;
+
+public class Login {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Login Page!");
+
+        System.out.print("Enter your username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Enter your password: ");
+        String password = scanner.nextLine();
+
+        if (authenticateUser(username, password)) {
+            System.out.println("Login successful! Welcome, " + username + ".");
+            //actions to be performed after successful login
+        } else {
+            System.out.println("Invalid username or password. Please try again.");
+            // handling unsuccessful login attempts
+        }
+      
+        
+    }
+
+    private static boolean authenticateUser(String username, String password) {
+        // implement your authentication logic.
+        
+        String expectedUsername = "ishwar";
+        String expectedPassword = "ishwar";
+
+        return username.equals(expectedUsername) && password.equals(expectedPassword);
+    }
+}
