0428be68f6c4ff76dfc0a1e99bc278a338fb4962
ishwarmishra <thisismeipm123@gmail.com>
2023-06-14 13:15:58 +0545

    commit again

--- Dashboard.java
+++ Dashboard.java
@@ -1,55 +1,40 @@
-
-package pft;
-
 import java.util.Scanner;
 
-
 public class Dashboard {
-    
-    public static void printMenu(String[] options){
-        System.out.println("Dashboard");
-        for (String option : options){
-            System.out.println(option);
-        }
-        System.out.print("Choose your option : ");
-    }
     public static void main(String[] args) {
-        String[] options = {"1- Income",
-                            "2- Expense",
-                            "3- exit",
-                            
-        };
         Scanner scanner = new Scanner(System.in);
-        int option = 1;
-        while (option!=3){
-            printMenu(options);
-            try {
-                option = scanner.nextInt();
-                switch (option){
-                    case 1: income(); break;
-                    
-                    case 2: expense(); break;
-                    
-                    case 3: exit(); 
-                    
-                }
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println("==== Dashboard Menu ====");
+            System.out.println("1. Income");
+            System.out.println("2. Expense");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+            switch (choice) {
+                case 1:
+                    System.out.println("You selected: Income");
+                    // Implement your logic for income
+                    break;
+                case 2:
+                    System.out.println("You selected: Expense");
+                    // Implement your logic for expense
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
             }
-            catch (Exception ex){
-                System.out.println("Please enter an integer value between 1 and " + options.length);
-                scanner.next();
-            }
+            System.out.println();
         }
-    }
 
-    private static void income() {
-        System.out.println("Welcome in Income Details");
+        scanner.close();
     }
-    private static void expense() {
-        System.out.println("Welcome in Expense Details");
-    }
-    private static void exit() {
-        System.out.println("You are exiting");
-    }
-
-    
 }
--- IncomeExpensedetails.java
+++ IncomeExpensedetails.java
@@ -1,26 +1,19 @@
-
 package pft;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
-
 public interface IncomeExpensedetails {
-    
+
     //for the incomeDetails class
     public String generateUniqueId();
-    void addIncome(String income,Date date);
+
+    void addIncome(String income, Date date);
+
     void displayIncomeSources();
+
     void showMenu();
-     
-     
-   
-     
-    
-     
-         
-    
-    
-        
+
 }
