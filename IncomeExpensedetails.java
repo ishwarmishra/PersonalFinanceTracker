fe2d3365941211a2529606ad94ddf9ec5f14630e
ishwarmishra <thisismeipm123@gmail.com>
2023-06-14 10:54:18 +0545

    commit changed

--- IncomeExpensedetails.java
+++ IncomeExpensedetails.java
@@ -1,15 +1,21 @@
 
 package pft;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
 
 public interface IncomeExpensedetails {
     
+    //for the incomeDetails class
      public String generateUniqueId();
      
-     void addIncome(String income);
+     void addIncome(String income,Date date);
      void displayIncomeSources();
      void showMenu();
      
-     void add();
+     
          
     
     
